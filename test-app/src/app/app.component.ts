b7032426e88a73bc7cba601406789e81e8e79ba7
salva <salvatv@hotmail.es>
2017-09-07 19:24:20 +0200

    funciones

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -11,16 +11,34 @@ export class AppComponent {
     'id': 1,
     'name': 'Juanky',
     'description': 'This item is a shit',
-    'stock': 5
+    'stock': 5,
+    'price': 23.56
   },
   {'id': 2,
   'name': 'Peter',
   'description': 'This item is a bullshit',
-  'stock': 10
+  'stock': 10,
+  'price': 23.56
   },
   {'id': 3,
   'name': 'Pako',
   'description': 'This item is a genius',
-  'stock': 0
+  'stock': 0,
+  'price': 23.56
   }];
+
+  totalItems() {
+    return this.myItems.length;
+  };
+
+  totalStock() {
+  //  let total = 0;
+  //  for (let entry of this.myItems) {      
+ //    total = entry.stock + total;      
+ // } 
+  //return total;   
+  
+  return this.myItems.reduce( (prev, current) => prev + current.stock, 0);
+  };
+
 }
