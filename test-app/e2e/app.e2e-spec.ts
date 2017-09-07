1660e766bcfc62eca11dbd2c2928bfcae20a909e
salva <salvatv@hotmail.es>
2017-09-07 16:07:48 +0200

    actualizado angular cli

--- test-app/e2e/app.e2e-spec.ts
+++ test-app/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { TestAppPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('test-app App', function() {
-  let page: TestAppPage;
+describe('test-app App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new TestAppPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
