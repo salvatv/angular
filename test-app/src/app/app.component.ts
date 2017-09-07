43d9a551bc88daf7866b3858ed830b909e39d50d
salva <salvatv@hotmail.es>
2017-09-07 18:02:49 +0200

    test

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'salvatv';
+  myItems = [{
+    'id': 1,
+    'name': 'Juanky',
+    'description': 'This item is a shit',
+    'stock': 5
+  },
+  {'id': 2,
+  'name': 'Peter',
+  'description': 'This item is a bullshit',
+  'stock': 10
+  },
+  {'id': 3,
+  'name': 'Pako',
+  'description': 'This item is a genius',
+  'stock': 0
+  }];
 }
